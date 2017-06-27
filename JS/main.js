5b25c335211533c1d6425c35cd4030d5801a65c2
olback <github@olback.net>
2017-06-27 18:44:54 +0200

    Added smooth scrolling

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+/*
+    Janne-Productions JavaScript
+*/
+
+// Smooth scrolling (Only supported in FireFox.)
+scrlTo = function(id) {
+    document.getElementById(id).scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+}
+
+console.log("'Main.js' loaded.");
+console.log('Found any problems or security flaws with this page? Open an issue here: https://github.com/oneofMOSSy/janneproductions/issues');
